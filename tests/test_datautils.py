013bcee50c2b9b72c2e0bc604ab7386fff1bcff2
williamtheodor <wls@braincapture.dk>
2023-01-18 16:57:46 +0100

    add toy_image for test datautils

--- tests/test_datautils.py
+++ tests/test_datautils.py
@@ -6,21 +6,16 @@ import pytest
 
 from src.data.data_utils import *
 
-@pytest.mark.skipif(len(os.listdir(_PATH_DATA + '/raw/images')) < 2, reason="Data files not found")
-def test_datautils():
+def test_datautils(slice_size = (512, 512)):
 
-    raw_dir = _PATH_DATA + '/raw'
-    name_dict = split_data(raw_dir)
-    image = torch.moveaxis(torchvision.io.read_image(raw_dir+'/images/'+name_dict['train'][0]), 0, -1)
+    toy_image = torch.ones(1,3,512,512)
 
-    slice_size = (512, 512)
-
-    idxs_height, idxs_width = get_slice_idxs(image.shape, slice_size)
-    image_slices = slice_image(image, slice_size + (3,), idxs_height, idxs_width)
+    idxs_height, idxs_width = get_slice_idxs(toy_image.shape, slice_size)
+    image_slices = slice_image(toy_image, slice_size + (3,), idxs_height, idxs_width)
 
     for i in range(image_slices.shape[0]):
         for j in range(image_slices.shape[1]):
             assert image_slices[i,j].shape == slice_size + (3,), "Image slice did not have correct dimensions"
 
-    unsliced_image = unslice_images(image_slices, image.shape, idxs_height, idxs_width)
-    assert unsliced_image.shape == image.shape, "Unsliced image dimensions did not match original image"
+    unsliced_image = unslice_images(image_slices, toy_image.shape, idxs_height, idxs_width)
+    assert unsliced_image.shape == toy_image.shape, "Unsliced image dimensions did not match original image"
